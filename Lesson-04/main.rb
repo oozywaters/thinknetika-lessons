1bae9934dad100eb1b2fcd632bfe0efd43fc1139
oozwaters <oozy.waters@gmail.com>
2018-01-30 17:01:34 +0300

    Add routes menu

--- Lesson-04/main.rb
+++ Lesson-04/main.rb
@@ -3,12 +3,14 @@ require_relative 'passenger_train'
 require_relative 'cargo_train'
 require_relative 'wagon'
 require_relative 'station'
+require_relative 'route'
 
 class App
   attr_reader :stations, :trains
 
   def initialize
-    @stations = []
+    @stations = [Station.new('Vnukovo'), Station.new('Belorusskaya')]
+    @routes = []
     @trains = { cargo: [], passenger: [] }
   end
 
@@ -23,6 +25,7 @@ class App
     station_name = gets.chomp
     if !stations.detect { |item| item.name == station_name }
       stations << Station.new(station_name)
+      puts "Station '#{station_name}' was added"
       display_main_menu
     else
       puts 'There is a station with such name already'
@@ -42,7 +45,7 @@ class App
       when 2
         type = 'cargo'
       else
-        puts 'There is no such type fo train.'
+        puts 'There is no such type of train.'
         display_train_menu
     end
     display_train_numer_menu(type)
@@ -57,20 +60,76 @@ class App
     display_main_menu
   end
 
+  def display_route_menu
+    puts 'Routes'
+    puts '1) Add route'
+    puts '2) Edit routes' unless @routes.empty?
+    puts '0) Back to Main Menu'
+    choice = gets.chomp.to_i
+    case choice
+      when 0 then display_main_menu
+      when 1 then display_add_route_menu
+      when 2
+        if @routes.empty?
+          puts 'There are no routes to edit yet'
+          display_route_menu
+        else
+          display_edit_routes_menu
+        end
+      else
+        puts 'There is no such option'
+        display_route_menu
+    end
+  end
+
+  def display_add_route_menu
+    puts 'Select starting station'
+    @stations.each_with_index { |item, index| puts "#{index}) #{item.name}" }
+    starting_station = @stations[gets.chomp.to_i]
+    if starting_station
+      puts 'Select ending station'
+      ending_stations = @stations.reject(starting_station)
+      ending_stations.each_with_index { |item, index| puts "#{index}) #{item.name}" }
+      end_station = ending_stations[gets.chomp.to_i]
+      return display_add_route_menu unless end_station
+      @routes << Route.new(starting_station, end_station)
+      display_route_menu
+    else
+      puts 'There is no such station'
+      display_add_route_menu
+    end
+  end
+
+  def display_routes_list
+    @routes.each_with_index do |item, index|
+      stations = item.stations.collect { |station| station.name }
+      puts "#{index}) #{stations.join(', ')}"
+    end
+  end
+
+  def display_edit_routes_menu
+    puts 'Select route to edit'
+    display_routes_list
+  end
+
   def display_main_menu
     puts 'What do you want?'
     puts '1) Add Station'
     puts '2) Add Train'
-    puts '3) Create a Route'
+    puts '3) Routes menu'
     puts '0) Exit'
     choice = gets.chomp.to_i
     case choice
-      when 1
-        display_station_menu
-      when 2
-        display_train_type_menu
-      when 0
-        exit
+      when 1 then display_station_menu
+      when 2 then display_train_type_menu
+      when 3
+        if @stations.size >= 2
+          display_route_menu
+        else
+          puts 'Please, add at least 2 stations first'
+          display_main_menu
+        end
+      when 0 then exit
       else
         puts 'There is no such option!'
         display_main_menu
