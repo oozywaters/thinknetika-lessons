68d657cf6af0126e9b424bf9ced539eaf14cb9d5
oozwaters <oozy.waters@gmail.com>
2018-01-30 12:40:04 +0300

    Change Train class

--- Lesson-04/train.rb
+++ Lesson-04/train.rb
@@ -1,5 +1,3 @@
-require './route.rb'
-
 class Train
   attr_reader :speed, :type, :wagons_qty
 
@@ -18,7 +16,7 @@ class Train
 
   def brake(amount = 30)
     new_speed_value = @speed - amount
-    @speed = new_speed_value <= 0 ? new_speed_value : 0
+    @speed = new_speed_value >= 0 ? new_speed_value : 0
   end
 
   def stop
@@ -35,8 +33,14 @@ class Train
 
   def set_route(route)
     if route.class == Route
+      self.current_station.send_train(self) if @route
       @route = route
+      go_to_station(0)
     end
+  end
+
+  def stations
+    @route.stations if @route
   end
 
   def previous_station
@@ -51,11 +55,19 @@ class Train
     @route.stations[@current_station_index] if @route
   end
 
+  def go_to_station(index)
+    if stations && stations[index] && index >= 0
+      self.current_station.send_train(self)
+      stations[index].accept_train(self)
+      @current_station_index = index
+    end
+  end
+
   def go_to_next_station
-    @current_station_index += 1 if @route && @current_station_index != @route.stations.size - 1
+    go_to_station(@current_station_index + 1)
   end
 
   def go_to_previous_station
-    @current_station_index -= 1 if @route && @current_station_index != 0
+    go_to_station(@current_station_index - 1)
   end
 end
