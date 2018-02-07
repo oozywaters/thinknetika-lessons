97070930dd6ebc594ac2fac91e579fe96662aa06
oozywaters <oozy.waters@gmail.com>
2018-02-07 20:19:51 +0300

    Add volume attribute to cargo wagon

--- Lesson-07/cargo_wagon.rb
+++ Lesson-07/cargo_wagon.rb
@@ -1,5 +1,19 @@
 class CargoWagon < Wagon
-  def initialize
+  attr_reader :volume, :free_space
+
+  def initialize(volume)
     super('cargo')
+    @volume = volume
+    @free_space = volume
+  end
+
+  def fill_wagon(volume)
+    return if @free_space.zero?
+    new_volume = @free_space - volume
+    @free_space = new_volume if new_volume >= 0
+  end
+
+  def occupied_space
+    @volume - @free_space
   end
 end
