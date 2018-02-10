7ec781357ab4a7580b73e94bf82d5e3fa91319be
oozywaters <oozy.waters@gmail.com>
2018-02-10 15:35:58 +0300

    Refactor routes menu

--- Lesson-08/main.rb
+++ Lesson-08/main.rb
@@ -2,48 +2,9 @@ require_relative 'menu'
 require_relative 'storage'
 require_relative 'stations_menu'
 require_relative 'trains_menu'
+require_relative 'routes_menu'
 
 class App < Menu
-  EDIT_ROUTE_MENU = {
-    title: 'Edit Route Menu',
-    items: {
-      '1' => {
-        name: 'Add Station',
-        action: :add_stations_to_route
-      },
-      '2' => {
-        name: 'Remove Station',
-        action: :remove_stations_from_route
-      },
-      '0' => {
-        name: 'Back to Routes Menu',
-        action: :display_main_menu
-      }
-    }
-  }
-
-  ROUTES_MENU = {
-    title: 'Routes Menu',
-    items: {
-      '1' => {
-        name: 'Add Route',
-        action: :add_route
-      },
-      '2' => {
-        name: 'Edit Route',
-        action: :edit_route
-      },
-      '3' => {
-        name: 'Remove Route',
-        action: :remove_route
-      },
-      '0' => {
-        name: 'Back to Main Menu',
-        action: :display_main_menu
-      }
-    }
-  }
-
   def title
     'Main Menu'
   end
@@ -60,17 +21,13 @@ class App < Menu
       },
       '3' => {
         name: 'Routes',
-        action: [:display_menu, ROUTES_MENU]
+        action: :show_routes_menu
       },
       '0' => {
         name: 'Exit',
         action: :close!
       }
     }
-  end
-
-  def run
-    display
   end
 
   private
@@ -83,72 +40,16 @@ class App < Menu
     TrainsMenu.new(@storage).display
   end
 
-  def add_route
-    all_stations = @storage.stations
-    if all_stations.size < 2
-      puts 'There are not enough stations to create a route'
-    else
-      puts 'Select starting station:'
-      starting_station = choose_item_from_array(all_stations)
-      puts 'Select ending station:'
-      rest_of_stations = all_stations.reject { |item| item == starting_station }
-      ending_station = choose_item_from_array(rest_of_stations)
-      @storage.add_route(Route.new(starting_station, ending_station))
-      puts "Route '#{starting_station.name} - #{ending_station.name}' was created."
-    end
-    display_menu(ROUTES_MENU)
-  end
-
-  def edit_route
-    if !@storage.routes?
-      puts 'There are no routes to edit. Please, create one.'
-      display_menu(ROUTES_MENU)
-    else
-      puts 'Select route to edit'
-      selected_route = choose_item_from_array(@storage.routes)
-      display_menu(EDIT_ROUTE_MENU, route: selected_route)
-    end
-  end
-
-  def remove_route
-    if !@storage.routes?
-      puts 'There are no routes to delete.'
-    else
-      puts 'Select route to delete:'
-      selected_route = choose_item_from_array(@storage.routes)
-      @storage.delete_route(selected_route)
-      puts "Route '#{selected_route.name}' was deleted"
-    end
-    display_menu(ROUTES_MENU)
-  end
-
-  def add_stations_to_route(route:)
-    stations_to_add = @storage.stations - route.stations
-    if stations_to_add.empty?
-      puts 'There are no stations to add. Please, create more stations.'
-    else
-      selected_station = choose_item_from_array(stations_to_add)
-      route.add_station(selected_station)
-      puts "Station #{selected_station.name} was added to '#{route.name}' route"
-    end
-    display_menu(EDIT_ROUTE_MENU, route: route)
-  end
-
-  def remove_stations_from_route(route:)
-    if route.way_stations.empty?
-      puts "There are no stations to remove in '#{route.name}' route"
-    else
-      selected_station = choose_item_from_array(route.way_stations)
-      route.remove_station(selected_station)
-      puts "Station #{selected_station.name} was removed from '#{route.name}' route"
-    end
-    display_menu(EDIT_ROUTE_MENU, route: route)
+  def show_routes_menu
+    RoutesMenu.new(@storage).display
   end
 
   def close!
     super
     puts 'See ya!!'
   end
+
+  alias_method :run, :display
 end
 
 storage = Storage.new
---
+++ Lesson-08/routes_menu.rb
@@ -0,0 +1,105 @@
+require_relative 'menu'
+require_relative 'route'
+
+class RouteEditMenu < Menu
+  def title
+    "Edit Route '#{route.name}'"
+  end
+
+  def items
+    {
+      '1' => {
+        name: 'Add Station',
+        action: :add_station
+      },
+      '2' => {
+        name: 'Remove Station',
+        action: :remove_station
+      },
+      '0' => {
+        name: 'Back to Routes Menu',
+        action: :close!
+      }
+    }
+  end
+
+  def route
+    @context[:route]
+  end
+
+  def display
+    super if route
+  end
+
+  private
+
+  def add_station
+    stations_to_add = @storage.stations - route.stations
+    return puts 'There are no stations to add. Please, create more stations.' if stations_to_add.empty?
+    selected_station = choose_item_from_array(stations_to_add)
+    route.add_station(selected_station)
+    puts "Station #{selected_station.name} was added to '#{route.name}' route"
+  end
+
+  def remove_stations_from_route
+    return puts "There are no stations to remove in '#{route.name}' route" if route.way_stations.empty?
+    selected_station = choose_item_from_array(route.way_stations)
+    route.remove_station(selected_station)
+    puts "Station #{selected_station.name} was removed from '#{route.name}' route"
+  end
+end
+
+class RoutesMenu < Menu
+  def title
+    'Routes Menu'
+  end
+
+  def items
+    {
+      '1' => {
+        name: 'Add Route',
+        action: :add_route
+      },
+      '2' => {
+        name: 'Edit Route',
+        action: :edit_route
+      },
+      '3' => {
+        name: 'Remove Route',
+        action: :remove_route
+      },
+      '0' => {
+        name: 'Back to Main Menu',
+        action: :close!
+      }
+    }
+  end
+
+  private
+
+  def add_route
+    return puts 'There are not enough stations to create a route' if @storage.stations.size < 2
+    puts 'Select Starting Station:'
+    starting_station = choose_item_from_array(@storage.stations)
+    puts 'Select Ending Station:'
+    rest_of_stations = @storage.stations.reject { |item| item == starting_station }
+    ending_station = choose_item_from_array(rest_of_stations)
+    @storage.add_route(Route.new(starting_station, ending_station))
+    puts "Route '#{starting_station.name} - #{ending_station.name}' was created."
+  end
+
+  def edit_route
+    return puts 'There are no routes to edit. Please, create one.' unless @storage.routes?
+    puts 'Select Route to Edit:'
+    selected_route = choose_item_from_array(@storage.routes)
+    RouteEditMenu.new(@storage, route: selected_route).display
+  end
+
+  def remove_route
+    return puts 'There are no routes to delete.' unless @storage.routes?
+    puts 'Select Route to Delete:'
+    selected_route = choose_item_from_array(@storage.routes)
+    @storage.delete_route(selected_route)
+    puts "Route '#{selected_route.name}' was deleted"
+  end
+end
