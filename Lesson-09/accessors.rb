89c3deb58155f7f2188aee2f90ac54af2439e358
oozwaters <oozy.waters@gmail.com>
2018-02-12 18:54:05 +0300

    Fix code review issues

--- Lesson-09/accessors.rb
+++ Lesson-09/accessors.rb
@@ -6,20 +6,21 @@ module Accessors
   module ClassMethods
     def attr_accessor_with_history(*names)
       names.each do |name|
-        class_eval { attr_reader name }
+        define_method(name) { instance_variable_get("@#{name}") }
         define_method("#{name}_history") { instance_variable_get("@#{name}_history") }
         define_method("#{name}=") do |value|
+          history = instance_variable_get("@#{name}_history")
+          current_value = instance_variable_get("@#{name}")
+          history ? history << current_value : instance_variable_set("@#{name}_history", [])
           instance_variable_set("@#{name}", value)
-          history = instance_variable_get("@#{name}_history") || []
-          instance_variable_set("@#{name}_history", history << value)
         end
       end
     end
 
     def strong_attr_accessor(name, type)
-      class_eval { attr_reader name }
+      define_method(name) { instance_variable_get("@#{name}") }
       define_method("#{name}=") do |value|
-        raise "#{value.class} provided to #{name} (#{type} required)" unless value.is_a?(type)
+        raise ArgumentError, "#{value.class} provided to #{name} (#{type} required)" unless value.is_a?(type)
         instance_variable_set("@#{name}", value)
       end
     end
